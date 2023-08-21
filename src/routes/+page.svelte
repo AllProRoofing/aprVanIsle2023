a052717ddd0c196028765401659de16702f59c64
vulbyte <insertanimationnamehere@gmail.com>
2023-08-21 12:34:31 -0700

    moved styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -67,5 +67,67 @@ you shouldn't see this 🤔
 
 <style>
     /* Your CSS styles here */
+:root {
+    --borderRadius: 10px;
+
+    --colorPrimary: #BF3131; 
+    --colorBgPrimary: #ddDDdd;
+    --colorBgSecondary: rgba(0, 0, 0 ,0.9);
+    --colorTextLight: #000;
+    --colorTextDark: #fff;
+
+    --fontPrimary: sans-serif;
+
+    --stdMargin: 1rem;
+    --stdPadding: 1rem;
+}
+
+:link {
+    color: var(--colorPrimary);
+    cursor: pointer;
+    text-decoration: none;
+}
+
+* {
+    border-radius: var(--borderRadius);
+    transition-duration: 200ms;
+}
+
+body {
+    aspect-ratio: auto;
+        height: auto;
+            min-height: 110vw;
+        width: auto;
+    background-color: var(--colorPrimary);
+    border-radius: var(--borderRadius);
+    font-family: sans-serif;
+    margin: 0px;
+    padding: 0px; 
+    overflow-x: hidden;
+}
+
+h1, h2, h3, h4, h5, h6 { 
+    background-color: var(--colorBgSecondary);
+    color: var(--colorTextDark);
+    margin: var(--stdMargin);
+    font-family: serif;
+        font-style: italic;
+        font-weight: bolder;
+        text-transform:uppercase;
+    padding: var(--stdPadding);
+    text-align: center;
+    width: calc((100%) - 2(var(--stdPadding)));
+}
+
+p {
+    background-color: var(--colorBgSecondary);
+    color: var(--colorTextDark); 
+    font-family: sans-serif;
+        letter-spacing: 1px;
+        line-height: 1.4rem;
+    margin: auto;
+    padding: var(--stdPadding);
+    max-width: 50rem;
+}
 </style>
 
